158b776edd0c4b44798961c2e7dcd3191b5cdb01
Al-amin Sheikh <azalamins@gmail.com>
2022-06-05 15:14:28 +0600

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a className="footer_logo" href="#home">
+        ALAMIN
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer-socials">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://facebook.com">
+          <FaInstagram />
+        </a>
+        <a href="https://facebook.com">
+          <FaTwitter />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Al Amin Sheikh, All rights reserved</small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
