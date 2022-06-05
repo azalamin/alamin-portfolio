dc93db3038c1257216e46d2a3020d30960c50754
Al-amin Sheikh <azalamins@gmail.com>
2022-06-05 07:55:22 +0600

    portfolio section done

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,54 @@
 import React from "react";
-import Img1 from "../../assets/digital.png";
+import Img2 from "../../assets/digital.png";
+import Img4 from "../../assets/doctors_portal.png";
+import Img3 from "../../assets/get-me-healthy.png";
+import Img1 from "../../assets/rinspeed.png";
 import "./Portfolio.css";
+
+const data = [
+  {
+    id: 1,
+    image: Img1,
+    title: "Rinspeed Car Dashboard Management",
+    github: "https://github.com/azalamin/Rinspeed-Car",
+    demo: "https://rinspeed-car.web.app/",
+  },
+  {
+    id: 2,
+    image: Img2,
+    title: "Digital Electronics (warehouse)",
+    github: "https://github.com/azalamin/digital-electronics-client",
+    demo: "https://digital-electronics-1f619.web.app/",
+  },
+  {
+    id: 3,
+    image: Img3,
+    title: "Get Me Healthy (GYM Trainer)",
+    github: "https://github.com/azalamin/get-me-healthy",
+    demo: "https://get-me-healthy.web.app/",
+  },
+  {
+    id: 4,
+    image: Img4,
+    title: "Doctors Portal (Booking Treatment)",
+    github: "https://github.com/azalamin/doctors-portal-client",
+    demo: "https://doctors-portal-a6cd1.web.app/",
+  },
+  {
+    id: 5,
+    image: Img1,
+    title: "Rinspeed Car Dashboard Management",
+    github: "https://github.com/azalamin/Rinspeed-Car",
+    demo: "https://rinspeed-car.web.app/",
+  },
+  {
+    id: 6,
+    image: Img1,
+    title: "Rinspeed Car Dashboard Management",
+    github: "https://github.com/azalamin/Rinspeed-Car",
+    demo: "https://rinspeed-car.web.app/",
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -8,25 +56,27 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio_container">
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Img1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item-cta">
-            <a href="https://www.github.com" className="btn">
-              Github
-            </a>
-            <a
-              href="https://www.github.com"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => (
+          <article key={id} className="portfolio_item">
+            <div className="portfolio_item-image">
+              <img src={image} alt="" />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio_item-cta">
+              <a href={github} className="btn">
+                Github
+              </a>
+              <a
+                href={demo}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
